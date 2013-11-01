88eab11404ce6596e2a7f72fa7c06486f393d0b0
xarus01 <xarus01@gmail.com>
2013-11-01 05:32:35 +0000

    query.js

--- server/web/app.js
+++ server/web/app.js
@@ -1,7 +1,16 @@
-var	express = require("express");
+var express = require("express");
+var io = require("socket.io").listen(80);
+//TODO: move socket.io code below express
+io.socket.on("connection", function(socket) {
+	/* for data income */
+	socket.on("income_event", function(data) {
+		/* push to redis */
+	});
+	/* else */
+	});
+		
 var application_root = "/home/ubuntu/Pancake/app/";
 var app = express();
-
 app.configure(function () {
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
@@ -9,5 +18,6 @@ app.configure(function () {
 	app.use(express.static(application_root));
 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
+
 app.listen(3000);
 console.log("listening on port 3000");
