b2e94f7c6a589af2252fa12a8c3b1aa7b75351db
kangho99 <KHY@Kenster-MacBook-Air.local>
2013-11-05 18:13:34 +0900

    socket.io server for midi file and composition upload onto db

--- server/web/app.js
+++ server/web/app.js
@@ -5,6 +5,9 @@ var socketio = require("socket.io");
 var io = socketio.listen(80); //Warning: listen EACCES
 var redis_port = "27071";
 var redis_host = "54.249.9.214";
+var fs = require("fs");
+var exec = require("child_process").exec;
+var util = require("util");
 
 function randomString() {
 	var chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz0123456789";
@@ -22,9 +25,7 @@ io.sockets.on("connection", function(socket) {
 	while (key in client.get("keys")) {
 		key = randomString();
 	}
-	socket.on("sendFirstMessage", function(FirstMessage) {
-		socket.emit("ready");
-	});
+	
 	/* in case modifying pre-composed files*/
 	socket.on("open", function(file_name) {
 		key = file_name;
@@ -38,9 +39,55 @@ io.sockets.on("connection", function(socket) {
 	socket.on("pop", function(index) {
 		client.pop(index);
 	});
-	socket.on("albumArt", function(name, buffer) {
-		/*something here*/
-	});
+
+	//------------saving midi data by either midiuploader or composer-----------
+
+	var MidiObject;
+	socket.on('saveMidiFile', function () {
+		socket.emit('startSave');
+
+		socket.on("midiData", function (data) {
+
+			MidiObject.title = data.title; //console.log(data.title);
+			MidiObject.playtime = data.playtime; //console.log(data.playtime);
+			MidiObject.owner = data.owner; //console.log(data.owner);
+			MidiObject.artist = data.artist; //console.log(data.artist);
+			MidiObject.description = data.description; //console.log(data.description);
+
+			//TODO: save data.MidiFile into DB and pass its Id
+			//console.dir(data.MidiFile);
+			//MidiObject.MidiFileId = 
+
+			//code to upload album art file and get its URL
+			var fs = require('fs');
+			//path to store uploaded files (NOTE: presumed you have created the folders)
+			//NOTE: data.albumArtName may collide with existing file;
+	        var fileName = __dirname + '/tmp/albumArt/' + data.albumArtName;
+
+	        fs.open(fileName, 'a', 0755, function(err, fd) {
+	        	//'a': probably append mode; 0755: permission
+	            if (err) throw err;
+	            fs.write(fd, data.albumArt, null, 'Binary', function(err, written, buff) {
+	                fs.close(fd, function() {
+	                    //console.log('File saved successful!');
+	                    //HERE:나중에 서버에서 이미지 리사이즈해서 보여줄 것을 생각한 place marker.
+
+	                	//TODO: get the url for uploaded album art
+	                	//MidiObject.albumArt = 'URL';
+	                })
+	            })
+	        })
+			
+		})
+
+		//TODO: code to push MidiObject into DB
+		//어제 이야기했던 MidiObject가 title이 아닌 id가 있어야 한다는 부분은
+		//아래 코드가 안바뀐것 같네. 그건 수정해주고, 생성한 id는 아래처럼.
+		//MidiObject.id = ???
+
+	}
+	//--------------------------------------------------------------------------
+
 	socket.on("save", function() {
 		socket.on("file_name", function(file_name, meta) {
 			if (file_name != key) {
