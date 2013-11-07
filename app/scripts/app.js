63e8f77bedf8afb376daf5863e6e1b76ea273bdb
VReality64 <VReality64@gmail.com>
2013-11-08 05:34:27 +0900

    FB Auth Test
    Done:
     - Login Button in nav bar
    
    Todo:
     - FB Auth Testing in Server

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,49 +1,72 @@
 'use strict';
 
-var app = angular.module('pancakeApp', ['ui.bootstrap']);
+var app = angular.module('pancakeApp', ['ui.bootstrap', 'ezfb']);
 
-app.config(function ($routeProvider, $locationProvider) {
+app.config(function ($routeProvider, $locationProvider, $FBProvider) {
   $locationProvider.html5Mode(false).hashPrefix('!');
+
+  $FBProvider.setInitParams({
+    appId: '1418276715068674'
+  });
 
   $routeProvider
     .when('/home', {
       templateUrl: 'views/main.html',
       controller: 'MainCtrl',
       title: 'Home',
-      isInEditor: false
+      isInEditor: false,
+      isLogged: false
     })
     .when('/editor', {
       templateUrl: 'views/editor.html',
       controller: 'EditorCtrl',
       title: 'Editor',
-      isInEditor: true
+      isInEditor: true,
+      isLogged: false
     })
     .when('/musiclist', {
       templateUrl: 'views/musiclist.html',
       controller: 'MusicListCtrl',
       title: 'Music List',
-      isInEditor: false
+      isInEditor: false,
+      isLogged: false
     })
     .when('/playlist', {
       templateUrl: 'views/playlist.html',
       controller: 'PlayListCtrl',
       title: 'Play List',
-      isInEditor: false
+      isInEditor: false,
+      isLogged: false
     })
     .when('/about', {
       templateUrl: 'views/about.html',
       controller: 'AboutCtrl',
       title: 'About',
-      isInEditor: false
+      isInEditor: false,
+      isLogged: false
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginCtrl',
+      title: 'Login',
+      isInEditor: false,
+      isLogged: false
     })
     .otherwise({
       redirectTo: '/home'
     });
 });
 
-app.run(function ($rootScope) {
+app.run(function ($rootScope, $location) {
   $rootScope.$on('$routeChangeSuccess', function(event, currentRoute) {
     $rootScope.title = currentRoute.title;
     $rootScope.isInEditor = currentRoute.isInEditor;
   });
+
+  $rootScope.login = function() {
+    if(!$rootScope.isLogged) {
+      $location.path('/login');
+    }
+  };
+
 });
